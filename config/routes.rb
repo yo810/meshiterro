360ef662f22471ef4204cdc8d27a8561af88d294
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-10-10 16:28:16 +0000

    [3-19] いいね機能追加,Favoriteモデル,ravoritesコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'post_comments/show'
+
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
